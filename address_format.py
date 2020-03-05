d3ff5aa90b413ceece809c85866873796085f117
rain2014 <303740673@qq.com>
2020-03-05 12:03:41 +0800

    抽取详细地址

--- address_format.py
+++ address_format.py
@@ -13,12 +13,46 @@ def init_jieba():
     jieba.load_userdict(os.path.join(father_path,'address/dict_new.txt'))
 
 
+def get_detailed_addr(src_address):
+    regex = {'town': '.{1,4}镇|.{1,4}街道', 'village': '.{1,4}村', 'abode': '.{1,4}宅',
+             'road_1': '.{2,4}路|.{2,3}大道', 'road_2': '.{2,4}路|.{2,3}大道', 'lane': '\d{1,4}弄',
+             'group': '\d{1,4}组','number': '\d{1,4}号', 'detail_address': '.*'}
+
+    # 指定区
+    fulldistrict = '浦东新区'
+    subdistrict = '浦东'
+
+    # 处理区描述不全问题
+    if re.match(fulldistrict, src_address):
+        src_address = re.sub('^' + fulldistrict, '', src_address)
+    elif re.match(subdistrict + '.{0,1}路|' + subdistrict + '大道', src_address):
+        pass
+    elif re.match(subdistrict, src_address):
+        src_address = re.sub('^' + subdistrict, '', src_address)
+
+    # 将括号内容放入detail_address
+    detail_address = re.findall('（(.*)）', src_address)
+    detail_address = '' if detail_address == [] else detail_address[0]
+
+    src_address = re.sub('（.*）', '', src_address)
+    for key in regex:
+        if key == 'detail_address':
+            break
+        addr = re.match(regex[key], src_address)
+        if addr:
+            end_position = re.match(regex[key], src_address).end()
+            src_address = src_address[end_position:]
+
+    detail_address = src_address + detail_address
+    return detail_address
+
+
 def addr_format(address, town):
     # 加载自动以词典
     init_jieba()
 
+    formated_addr = {'街道': town}
     ws = posseg.lcut(address)
-    formated_addr = {'街道':town}
     roads = []
     for w in ws:
         # 多个村,只保留一个最后一个
@@ -37,6 +71,10 @@ def addr_format(address, town):
         formated_addr['弄'] = re.search('\d{1,4}弄', address).group()
     if re.search('\d{1,5}号',address):
         formated_addr['号'] = re.search('\d{1,4}号', address).group()
+
+    # 获取详细地址
+    detailed_addr = get_detailed_addr(address)
+    formated_addr['详细地址'] = detailed_addr
 
     results = {}
     results['header'] = list(formated_addr.keys())
@@ -74,25 +112,11 @@ def get_poi(coordinate, entity):
             if len(set(poi['name']).intersection(set(entity))) > 2:
                 addr_type = poi['type']
                 return addr_type
-    #通过实体中的关键字进行类别判定
-    addr_type = get_type_by_keyword(entity)
-    return addr_type 
-
-
-def address_info(address):
-    # address = address_clean(address)
-    # entity, coordinate = get_entity_and_coordinate(address)
-    # addr_type = get_poi(coordinate, entity)
-    formated_address = get_formated_addr(address)
-    
-    data = dict()
-    data['formated_address'] = formated_address
-    # data['entity'] = entity if entity else ''
-    # data['addr_type'] = addr_type
-    return data  
 
 
 if __name__ == '__main__':
-    result = addr_format('浦东新区高桥镇镇南村西浜头178号','大场镇')
+    result = addr_format('外环线内过沪南公路入口200米','大场镇')
     print(result)
+
+
 
